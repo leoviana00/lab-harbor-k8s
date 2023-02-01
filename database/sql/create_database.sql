3e7a188960928db48e7419b2cf5f69fe5a4eb930
Leonardo Viana <leonardoviana00@hotmail.com>
2023-02-01 17:01:59 -0300

    feature(database): ajustando database

--- database/sql/create_database.sql
+++ database/sql/create_database.sql
@@ -1,3 +1,11 @@
-create DATABASE registry;
-create DATABASE notary_signer;
-create DATABASE notary_server;
+--  CRIPTOGRAFIA DE SENHA
+-- ALTER SYSTEM SET password_encryption = 'md5';
+-- SELECT pg_reload_conf();
+ALTER USER root WITH PASSWORD 'password';
+
+-- CREATE DATABASES
+CREATE DATABASE registry;
+CREATE DATABASE notary_signer;
+CREATE DATABASE notary_server;
+
+
