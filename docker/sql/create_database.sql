8bacef3bc73bde90ce7eff4a8ee7841c3aa43651
Leonardo Viana <leonardoviana00@hotmail.com>
2023-03-16 15:41:29 -0300

    feat(harbor-docker): add docker-compose harbor

---
+++ docker/sql/create_database.sql
@@ -0,0 +1,9 @@
+--  CRIPTOGRAFIA DE SENHA
+-- ALTER SYSTEM SET password_encryption = 'md5';
+-- SELECT pg_reload_conf();
+ALTER USER root WITH PASSWORD 'password';
+
+-- CREATE DATABASES
+CREATE DATABASE registry;
+CREATE DATABASE notary_signer;
+CREATE DATABASE notary_server;
